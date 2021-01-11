4f5f66564b616f4acd6240b6ca7fdf4f4efb0ffd
EthanDevelops <66741050+EthanDevelops@users.noreply.github.com>
2021-01-11 11:12:47 -0500

    Update TestMinecraftPlugin.java

--- src/main/java/me/bennun/testminecraftplugin/TestMinecraftPlugin.java
+++ src/main/java/me/bennun/testminecraftplugin/TestMinecraftPlugin.java
@@ -5,12 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TestMinecraftPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("onEnable was called!");
+        getLogger().info("onEnable was called EJM-2!");
         // getLogger().warning("This is a test error that is called when onEnable was called.");
     }
     @Override
     public void onDisable() {
-        getLogger().info("onDisable was called!");
+        getLogger().info("onDisable was called EJM-2!");
         // getLogger().warning("This is a test error that is called when onDisable was called.");
     }
 }
